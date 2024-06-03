19c4b3a8d0ffae523068d2db574f5403cfd95a17
Mario Reder <mario.reder@pm.me>
2024-06-03 10:12:28 +0200

    build script

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+cd "`dirname $0`"
+
+cargo build --release --target wasm32-unknown-unknown
+cp target/wasm32-unknown-unknown/release/*.wasm ./res/
+
+wasm-opt -O4 res/rewarder.wasm -o res/rewarder.wasm --strip-debug --vacuum
