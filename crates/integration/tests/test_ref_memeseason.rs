d1a40caa80687c85bc2b2fbd1212f37f53fd1519
pysrxyz <gm@pysr.xyz>
2024-06-18 15:40:39 +0700

    test: score from multiple farm

--- crates/integration/tests/test_ref_memeseason.rs
+++ crates/integration/tests/test_ref_memeseason.rs
@@ -3,7 +3,7 @@ use helpers::{
     setup::{setup, SetupResult},
     view, Ether,
 };
-use near_sdk::json_types::U128;
+use near_sdk::{json_types::U128, NearToken};
 
 mod helpers;
 
@@ -85,6 +85,74 @@ async fn test_farmer_can_claim() -> anyhow::Result<()> {
             .await
             .is_ok(),
         "Expected successful claim"
+    );
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_multiple_farms() -> anyhow::Result<()> {
+    let worker = near_workspaces::sandbox().await?;
+    let SetupResult {
+        shitzu,
+        rewarder,
+        accounts,
+        memeseason,
+        xref,
+        xref_staking,
+        shitzu_staking,
+        lp_token,
+        lp_staking,
+        nft,
+        ..
+    } = setup(&worker).await?;
+
+    let [alice, _bob, ..] = &accounts[..] else {
+        anyhow::bail!("Expected at least 4 accounts, got {}", accounts.len())
+    };
+
+    let [alice_nft_token_id, ..] = &call::mint_nft(&alice, nft.id(), 1).await?[..] else {
+        anyhow::bail!("Expected at least 1 NFT token, got 0")
+    };
+
+    call::stake(alice, rewarder.id(), nft.id(), &alice_nft_token_id.token_id).await?;
+
+    let shitzu_amount: U128 = Ether::from(900).into();
+    call::storage_deposit(&shitzu, alice, None, None).await?;
+    call::storage_deposit(&shitzu, alice, Some(shitzu_staking.id()), None).await?;
+    call::storage_deposit(&shitzu_staking, alice, None, None).await?;
+
+    call::mint_token(&shitzu, alice.id(), shitzu_amount).await?;
+    // 50 + 6 points
+    call::stake_seed(&alice, shitzu.id(), shitzu_amount, shitzu_staking.id()).await?;
+
+    let xref_amount: U128 = Ether::from(100).into();
+    call::storage_deposit(&xref, alice, None, None).await?;
+    call::storage_deposit(&xref, alice, Some(xref_staking.id()), None).await?;
+    call::storage_deposit(&xref_staking, alice, None, None).await?;
+
+    call::mint_token(&xref, alice.id(), xref_amount).await?;
+    // 100 + 10 points
+    call::stake_seed(&alice, xref.id(), xref_amount, xref_staking.id()).await?;
+
+    let lp_amount: U128 = U128::from(10_u128.pow(22)); // 0.01 * 10^24
+    call::storage_deposit(&lp_token, alice, None, None).await?;
+    call::storage_deposit(&lp_token, alice, Some(lp_staking.id()), None).await?;
+    call::storage_deposit(&lp_staking, alice, None, None).await?;
+
+    call::mint_token(&lp_token, alice.id(), lp_amount).await?;
+    // 50 + 10 points
+    call::stake_seed(&alice, lp_token.id(), lp_amount, lp_staking.id()).await?;
+
+    call::claim_ref_memeseason(&alice, memeseason.id()).await?;
+    let score = view::score_of(&rewarder, alice_nft_token_id.token_id.clone()).await?;
+    let expected_score = U128(226 * 10_u128.pow(18));
+
+    assert!(
+        score == expected_score,
+        "Expected score to be {}, got {}",
+        expected_score.0,
+        score.0
     );
 
     Ok(())
