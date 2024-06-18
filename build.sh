b00ed6047c3c7377218203e3396f4be2911e4d21
pysrxyz <gm@pysr.xyz>
2024-06-18 09:32:02 +0700

    chore: add memeseason build command

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ cargo build --release --target wasm32-unknown-unknown
 cp target/wasm32-unknown-unknown/release/*.wasm ./res/
 
 wasm-opt -O4 res/rewarder.wasm -o res/rewarder.wasm --strip-debug --vacuum
+wasm-opt -O4 res/memeseason.wasm -o res/memeseason.wasm --strip-debug --vacuum
